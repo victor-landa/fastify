a4a76e781cb27fb5906e6566a72dd64fd3a77b28
victor-landa <victorlanda.dev@gmail.com>
2021-06-21 22:02:48 -0500

    fastify

---
+++ Items.js
@@ -0,0 +1,10 @@
+let items = [
+  {id: '1', name: 'Item One'},
+  {id: '2', name: 'Item Two'},
+  {id: '3', name: 'Item Three'},
+  {id: '4', name: 'Item Four'},
+  {id: '5', name: 'Item Five'},
+  {id: '6', name: 'Item Six'}
+];
+
+module.exports = items;
---
+++ routes/items.js
@@ -0,0 +1,99 @@
+const { getItems, getItem, addItem, deleteItem, updateItem } = require('../controllers/items');
+
+// Item schema
+const Item = {
+  type: 'object',
+  properties: {
+    id: {type: 'string'},
+    name: {type: 'string'}
+  }
+}
+
+function itemRoutes(fastify, options, done) {
+  // Options for get all items
+  const getItemsOpts = {
+    schema: {
+      response: {
+        200: {
+          type: 'array',
+          items: Item
+        }
+      }
+    },
+    handler: getItems
+  };
+
+  const getItemOpts = {
+    schema: {
+      response: {
+        200: Item
+      }
+    },
+    handler: getItem
+  };
+
+  const postItemOpts = {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['name'],
+        properties: {
+          name: { type: 'string' }
+        }
+      },
+      response: {
+        201: Item
+      }
+    },
+    handler: addItem
+  }
+
+  const deleteItemOpts = {
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            message: { type: 'string' }
+          }
+        }
+      }
+    },
+    handler: deleteItem
+  }
+
+  const updateItemOpts = {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['name'],
+        properties: {
+          name: { type: 'string' }
+        }
+      },
+      response: {
+        200: Item
+      }
+    },
+    handler: updateItem
+  }
+
+  // Get all items
+  fastify.get('/items', getItemsOpts);
+  
+  // Get single item
+  fastify.get('/items/:id', getItemOpts);
+
+  // Add item
+  fastify.post('/items', postItemOpts);
+
+  // Delete item
+  fastify.delete('/items/:id', deleteItemOpts);
+
+  // Update item
+  fastify.put('/items/:id', updateItemOpts);
+
+  done();
+}
+
+module.exports = itemRoutes;
